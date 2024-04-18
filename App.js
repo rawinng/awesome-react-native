8f97b0421f58d85f86c881195562ea18b70e2784
Rawin Ngamloet <rawin.ngamloet@scb.co.th>
2024-04-19 06:49:26 +0700

    Add code for react-native expo

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hope the app is working well</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
